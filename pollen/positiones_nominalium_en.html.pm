ecae880593e2fa62a1fd9f2e45233a23e45321a6
Roxane Noël <roxane.noel92@gmail.com>
2023-07-04 11:07:31 +0100

    Finished writing all positiones

--- pollen/positiones_nominalium_en.html.pm
+++ pollen/positiones_nominalium_en.html.pm
@@ -13,6 +13,16 @@ These are our positions concerning universals.
 ◊item{Fourth, we say, with Zeno, that it does not appear that something ◊addition{can} move, to which Aristotle disagrees at the end of the sixth ◊addition{book} of Physics.}
 ◊item{We state that there is no action, except via pores (◊original-latin{per poros}).}
 ◊item{Again, we say, with Zeno, that being is said univocally about those ◊addition{things} which are (◊original-latin{de hiis quae sunt}).}
-◊item{Moreover, we say that there are only two universals, namely ◊original-latin{genus} and ◊original-latin{species}.}
+◊item{Moreover, we say that there are only two universals, namely ◊original-latin{genus} and ◊original-latin{species};}
+◊item{and that some accident is in a subject which neither denominates the subject ◊original-latin{simpliciter}, nor according to the "what" (◊original-latin{secundum quid});}
+◊item{and that ◊uncertain{"accident"} contains under itself all universals.}
+◊item{Again, we say that grammar, dialectic, and rhetoric are a single science (◊original-latin{unam scientiam}), against the division of Al-Farabi;}
+◊item{besides this we disagree with what Priscian is saying, ◊addition{namely} that a syllab cannot ◊uncertain{be had in a meter at many times (◊original-latin{plura tempora}), because we posit that a syllab can be had in an infinite time.}}
+◊item{Also we disagree with the same ◊addition{(Priscian)} and agree with the Stoics, who posited that an interjection has to be included among adverbs.}
+◊item{Again, we posit that it is insufficient, because he did not teach the species of figures of speech (◊original-latin{topoi}).}
+◊item{Likewise, we posit that no construction is faulty.}
+◊item{Similarly, against Aristotle, we say that the book On Interpretation is thoroughly ◊uncertain{superfluous/overabundant}.}
+◊item{And again that it is ◊uncertain{diminished}, because it did not thoroughly teach the art of dividing,}
+◊item{and that all science is demonstrative, not topical ◊addition{(of the topics)}.}
 }
 }
